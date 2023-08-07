ddc3ceb116ed0acc260a278116f43300b1394964
Alwaz Shahid <52915358+alwaz-shahid@users.noreply.github.com>
2023-08-07 18:03:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import getpass
 
 def permission_string(permissions):
     return "".join([
-        "r" if permissions & 0o400 else "-",
-        "w" if permissions & 0o200 else "-",
-        "x" if permissions & 0o100 else "-",
+        "read" if permissions & 0o400 else "-",
+        "write" if permissions & 0o200 else "-",
+        "execute" if permissions & 0o100 else "-",
     ])
+
+def display_manual():
+    click.echo("Welcome to the Partition and Disk Permission Manager for Linux!")
+    click.echo("===============================================================\n")
+    click.echo("This tool allows you to view and modify permissions for partitions and disks.")
+    click.echo("You can use natural language like 'r' for read, 'w' for write, and 'e' for execute when setting permissions.")
+    click.echo("Here are some usage examples:")
+    click.echo("  - 'r w e' or '7': Give read, write, and execute permissions.")
+    click.echo("  - 'r e' or '5': Give read and execute permissions.")
+    click.echo("  - 'r' or '4': Give read-only permissions.\n")
 
 @click.command()
 def main():
-    click.echo("Partition Permission Manager for Linux")
-    click.echo("======================================\n")
-    
+    display_manual()
+
     current_user = getpass.getuser()
     click.echo(f"Current User: {current_user}\n")
 
@@ -42,10 +51,18 @@ def main():
             click.echo(f"  Group Permissions: {permission_string(permissions.st_mode & 0o070)}")
             click.echo(f"  Others Permissions: {permission_string(permissions.st_mode & 0o007)}\n")
 
-            choice = click.prompt("Do you want to change permissions? (yes/no): ").strip().lower()
-            if choice == "yes":
-                new_permissions = click.prompt("Enter new permissions (e.g., 755):", default="755")
-                os.chmod(partition_path, int(new_permissions, 8))
+            choice = click.confirm("Do you want to change permissions?", default=False)
+            if choice:
+                new_permissions = click.prompt("Enter new permissions (e.g., r w e = 7):", default="7")
+                permission_value = 0
+                if "r" in new_permissions:
+                    permission_value |= 0o400
+                if "w" in new_permissions:
+                    permission_value |= 0o200
+                if "e" in new_permissions:
+                    permission_value |= 0o100
+
+                os.chmod(partition_path, permission_value)
                 click.echo("Permissions updated successfully.\n")
             else:
                 click.echo("Permissions remain unchanged.\n")
